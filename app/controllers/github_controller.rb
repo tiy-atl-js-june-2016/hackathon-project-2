33779d6dcd017b8a7496c6867a8db047663d5f5a
Tim Whitacre <tim@timw.co>
2016-07-29 13:00:41 -0400

    added some error messages for the github route

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,6 +1,6 @@
 class GithubController < ApplicationController
 
-  before_filter :github_connect
+  before_action :github_connect
 
   def github_connect
     @github = Github.new login:ENV['GITHUB_USERNAME'], password: ENV['GITHUB_TOKEN']
@@ -8,14 +8,23 @@ class GithubController < ApplicationController
 
   def index
 
+    # Get user by username
     user = User.find_by(username: params['username'])
-    profile = Profile.find_by(user_id: user.id)
+    if (user)
+      # Get profile by user name then make sure user actually has created a profile
+      profile = Profile.find_by(user_id: user.id)
+      if (profile && profile.github_username)
+        @user = @github.users.get user: profile.github_username
+        @repos = @github.repos.list user: profile.github_username
+        @orgs = @github.orgs.list user: profile.github_username
+        render "index.json.jbuilder"
+      else
+        render json: { error: 'User is either missing a profile or their github username is empty' }, status: :unprocessable_entity
+      end
+    else 
+      render json: { error: "No user with username `#{params['username']}`" }, status: :unprocessable_entity
+    end
 
-    @user = @github.users.get user: profile.github_username
-    @repos = @github.repos.list user: profile.github_username
-    @orgs = @github.orgs.list user: profile.github_username
-
-    render "index.json.jbuilder"
   end
 
 end
